290b69a29215ec82a95098f9ddef1f63e72bc60e
baconhair <38001046+baconhair@users.noreply.github.com>
2018-06-16 12:54:02 +0800

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,3 +1,11 @@
-exports.run = (bot, message, args) => {
-f
+exports.run = (bot, message, args, ops) => {
+
+  if (message.author.id !== ops.ownerID) return message.channel.send('This is a owner only command');
+  
+  try {
+    delete require.cache[require.resolve(`./${args[0]}.js`)];
+  } catch(e) {
+    return message.channel.send(`Cant reload: ${args[0]}`);
+  }
+  message.channel.send(`Successfully reloaded: ${args[0]}`)
 }
